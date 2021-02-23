82f0e05d8f3c51a204111ed56c5ec97219113a84
Alexandr-Salienko <79051157+Alexandr-Salienko@users.noreply.github.com>
2021-02-23 22:26:18 +0400

    Delete projects/mio-kegel directory

--- projects/mio-kegel/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(
-	function(){
-		$('.site-header__main-navigation-submenu-toggler').on('click', 
-			function(){
-				$(this).closest('li').children('ul').slideToggle(300);
-			}
-		)
-	}
-)
